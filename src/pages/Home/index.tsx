cf8a9f96e776eae8f087e10751bc35e8e7969861
Daniel Bergholz <bergholz.daniel@gmail.com>
2021-10-26 18:56:48 -0300

    wip: Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,32 @@
+import { Box, Text } from '@chakra-ui/react'
 import Search from '../../components/Search'
 
 const Home = () => {
   return (
     <>
-      <Search />
+      <Box marginX="20px">
+        <Search mt="50px" />
+
+        <Text fontWeight="500" fontSize="24px" lineHeight="28px" my="20px">
+          Hi,{' '}
+          <Text as="span" color="pink">
+            Daniel Bergholz
+          </Text>
+          <Box display="inline" ml="11px">
+            &#128075;
+          </Box>
+        </Text>
+
+        <Text
+          lineHeight="21px"
+          fontSize="18px"
+          letterSpacing="0.5px"
+          color="gray.700"
+          mb="15px"
+        >
+          Discover new book
+        </Text>
+      </Box>
     </>
   )
 }
