a67d7b8e6b285455dceaee63d00f2e82d7aea481
Daniel Bergholz <bergholz.daniel@gmail.com>
2021-10-28 11:16:16 -0300

    feat(OptionsBar): add copy to clipboard on share button

--- src/components/OptionsBar/index.tsx
+++ src/components/OptionsBar/index.tsx
@@ -1,9 +1,37 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Flex, Text } from '@chakra-ui/layout'
-import { Icon } from '@chakra-ui/react'
+import { Icon, useToast } from '@chakra-ui/react'
+import { useCallback } from 'react'
 import { BiHeadphone } from 'react-icons/bi'
 import { FiBookOpen, FiShare } from 'react-icons/fi'
 
 const OptionsBar = () => {
+  const toast = useToast()
+
+  const copyToClipboard = useCallback(async () => {
+    try {
+      await navigator.clipboard.writeText(window.location.href)
+      toast({
+        title: 'URL copiada com sucesso!',
+        description:
+          'O link atual do site foi copiado para a área de transferência',
+        status: 'success',
+        position: 'top-right',
+        duration: 5000,
+        isClosable: true,
+      })
+    } catch (err) {
+      toast({
+        title: 'Erro na cópia da URL',
+        description: 'Para compartilhar o link atual, basta copiar a URL',
+        status: 'error',
+        position: 'top-right',
+        duration: 5000,
+        isClosable: true,
+      })
+    }
+  }, [])
+
   return (
     <Flex
       position="fixed"
@@ -35,7 +63,7 @@ const OptionsBar = () => {
 
       <Text color="gray.150">|</Text>
 
-      <Flex alignItems="center">
+      <Flex alignItems="center" cursor="pointer" onClick={copyToClipboard}>
         <Icon as={FiShare} color="gray.150" mr="10px" />
         <Text fontWeight="700" fontSize="14px">
           Share
